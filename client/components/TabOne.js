be8d07aa12478b8cbf649c1f881c0260c7add747
Joshua Grossman <Josh1794@gmail.com>
2020-04-01 13:31:56 -0400

    first commit

--- client/components/TabOne.js
+++ client/components/TabOne.js
@@ -1,14 +1,80 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Gyroscope } from "expo-sensors";
 import { Text, Layout } from "react-native-ui-kitten";
 
 const TabOne = props => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    _toggle();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      _unsubscribe();
+    };
+  }, []);
+
+  const _toggle = () => {
+    if (this._subscription) {
+      _unsubscribe();
+    } else {
+      _subscribe();
+    }
+  };
+
+  const _slow = () => {
+    Gyroscope.setUpdateInterval(1000);
+  };
+
+  const _fast = () => {
+    Gyroscope.setUpdateInterval(16);
+  };
+
+  const _subscribe = () => {
+    this._subscription = Gyroscope.addListener(gyroscopeData => {
+      setData(gyroscopeData);
+    });
+  };
+
+  const _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  let { x, y, z } = data;
   return (
-    <Layout style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-    </Layout>
+    <View style={styles.sensor}>
+      <Text style={styles.text}>Gyroscope:</Text>
+      <Text style={styles.text}>
+        x: {round(x)} y: {round(y)} z: {round(z)}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={_toggle} style={styles.button}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
+
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 100) / 100;
+}
 
 const styles = StyleSheet.create({
   container: {
